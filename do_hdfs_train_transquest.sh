85ce1ce75b8c5229af3d8659cf7c0ca642ba90f5
yanyiming.mello <yanyiming.mello@bytedance.com>
2022-10-02 10:30:58 +0800

    update

---
+++ do_hdfs_train_transquest.sh
@@ -0,0 +1,24 @@
+# 读取命令
+echo "#=========================== print cmd ===========================#"
+lang_pair=`echo "$@" | awk -F'+' '{print $1}'`  # en-zh
+echo ${lang_pair}
+
+# 复制数据
+echo "#=========================== get data ===========================#"
+hdfs dfs -get /home/byte_arnold_lq_mlnlc/user/yanyiming.mello/data/mlqe-pe_word_level ./
+mv mlqe-pe_word_level data
+
+# 复制xlm roberta预训练模型
+echo "#=========================== get xlmr ===========================#"
+mkdir -p transformers
+hdfs dfs -get /home/byte_arnold_lq_mlnlc/user/yanyiming.mello/transformers/xlm-roberta ./transformers
+
+# 训练啦
+echo "#=========================== begin training ===========================#"
+python3 -m mlqe_word_level.microtransquest -l $lang_pair # en-zh
+
+# 训完上传输出到hdfs啦
+echo "Finish training, uploading outputs to hdfs"
+mv train_result train_result_$lang_pair
+hdfs dfs -put train_result_$lang_pair /home/byte_arnold_lq_mlnlc/user/yanyiming.mello/qe_shortcut/transquest_model
+echo "Finish uploading outputs to hdfs"
